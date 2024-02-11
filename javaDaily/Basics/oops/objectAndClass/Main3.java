50b46b1bdccf73b056adf2db95d9b9d967877de9
jojiseb <73522959+jojiseb@users.noreply.github.com>
2024-02-11 23:41:21 +0530

    exploring Objects, how they are allocated in memory etc.

---
+++ javaDaily/Basics/oops/objectAndClass/Main3.java
@@ -0,0 +1,31 @@
+package objectAndClass;
+
+import java.util.Arrays;
+
+class Employee { //instance variables - outside methods, inside class
+    int eNo; //3 instance variables / states - variables inside object
+    String empName; //We access instance varibles using dot oprtr (.);
+    String empDept = "Product"; // objectReference.propertyName
+}
+
+public class Main3 {
+    public static void main(String[] args) {
+        //Employee employee; //Since, its local it says a non-initialised,for that below eg..
+        Employee[] employees = new Employee[2]; // Alternative example to show the value of a reference variable
+        System.out.println(Arrays.toString(employees)); // which is not initialising an object -- null is the default value
+        Employee employee = new Employee(); // LHS happens @ Compile time -- reference variable created
+        //RHS happens @ run-time -- allocates m/y @ rum-time
+
+        System.out.println("Emp No : "+employee.eNo);    // 0
+        System.out.println("Name : "+employee.empName); // null
+
+        employee.eNo = 23;
+        employee.empName = "Sal"; //Initialising values..
+
+        System.out.println("Emp No : "+employee.eNo); //23
+        System.out.println("Name : "+employee.empName); //Sal
+
+        System.out.println("Dept : "+employee.empDept); // Product -- default value of instance variable
+    }
+    // new - allocates m/y @ run-time (dynamic m/y allocation) & returns a reference to it
+}
