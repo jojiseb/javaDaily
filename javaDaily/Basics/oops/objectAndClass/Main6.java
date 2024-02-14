d41215b6904e4aaaa76c7439efc95fb7cc070418
jojiseb <73522959+jojiseb@users.noreply.github.com>
2024-02-14 23:49:18 +0530

    Learnt more about constructors, why they are initialised, usage of this keyword etc.

--- javaDaily/Basics/oops/objectAndClass/Main6.java
+++ javaDaily/Basics/oops/objectAndClass/Main6.java
@@ -4,11 +4,26 @@ class Cup {
     int cupNo;
     double width, breadth, height;
 
+    //"this" keyword means what object you are referring to
+//Examples of multiple constructors
+
+    //"this" keyword gets replaced by reference variable, where-ever its used
+    //Its same as using dot operator to assign value inside object to instance variable
+    //What happens inside constructor or a method call where "this" is used
+    //this.propetyName = refVariable.propertyName
+
     Cup() {
         this.cupNo = 12;
         this.breadth = 10;
         this.width = 11;
         this.height = 15;
+    }
+
+    Cup(Cup cup) {
+        this.cupNo = cup.cupNo;
+        this.height = cup.height;
+        this.width = cup.width;
+        this.breadth = cup.breadth;
     }
 
     Cup(double w, double b, double h) {
@@ -18,7 +33,7 @@ class Cup {
     }
 
     Cup(int cupNo, double w, double b, double h) {
-        this(w, b, h);
+        this(w, b, h);  //calling another constructor inside another constructor
         this.cupNo = cupNo;
     }
 
@@ -37,5 +52,10 @@ public class Main6 {
         System.out.println(cup);
         System.out.println(cup1);
         System.out.println(cup2);
+
+        Cup cup3 = new Cup(cup2);
+
+        System.out.println(cup3);
+        System.out.println("cup2 hash : "+cup2.hashCode()+", cup3 hash : "+cup3.hashCode());
     }
 }
