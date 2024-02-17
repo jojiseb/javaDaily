afe05dc83a72a856f9116890ff7035393a2a1085
jojiseb <73522959+jojiseb@users.noreply.github.com>
2024-02-17 23:51:03 +0530

    -Learnt lil bit about wrapper classes, why we use them - Learnt a bit of examples

--- javaDaily/Basics/Wrapper_Examples/Main.java
+++ javaDaily/Basics/Wrapper_Examples/Main.java
@@ -4,6 +4,8 @@ public class Main {
         int c = 56;
 
         Integer v = 850;
+        // Wrapper classes were introduced to make the extended functionalities to primitive types,
+        // to truly use them like objects
 
         Integer b = new Integer(90);
 
@@ -11,5 +13,28 @@ public class Main {
                         //So, predictable functionality as integers;
 
         System.out.println(v);  // Can apply dot operator as this is an Object
-    }                   //A class is just group of props and behaviours
+
+        // A class is just group of props and behaviours, so just like that wrapper classes consist of them too
+        // we can use them as needed
+
+        // Wrapper classes are for basically using primitive types as objects
+
+        int a = 11;
+
+        System.out.println("Before swapping : \na : "+a+"\nc : "+c);
+        swap(a,c);                                      // Here, Swapping doesn't work as primitive types are pass-by-value
+        System.out.println("After swapping : \na : "+a+"\nc : "+c); // So, Even if swapping happens, it is just scoped to the swap(),
+                                                                    // and initial values before swapping remains retained
+
+    }
+
+    static void swap(int a , int c) {
+        int temp = a;
+        a = c;
+        c  = temp;
+    }
+
+    static class MyClass {
+
+    }
 }
