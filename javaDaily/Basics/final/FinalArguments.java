22a475c84a90ddc1e1f6a962cd31efc541c8e804
jojiseb <73522959+jojiseb@users.noreply.github.com>
2024-02-26 23:49:17 +0530

    - Learning finally keyboard and its uses

--- javaDaily/Basics/final/FinalArguments.java
+++ javaDaily/Basics/final/FinalArguments.java
@@ -1,13 +1,19 @@
 
 class Args {
     int y;
-    Args(int y) {
+    Args(final int y) {
         this.y = y;
-        y = 7;
+//        y = 7; --> Can't do this as variable y is passed as a final argument, and since final variable is initialized once, can't initialize again.
     }
 
     void output() {
         System.out.println("Y : "+this.y);
+    }
+
+    void test(final String jar) {
+        System.out.println("Hello World.."+jar);
+//        jar = "New";  //throws issues from using final keyword
+                        //since a final
     }
 }
 
@@ -15,5 +21,6 @@ public class FinalArguments {
     public static void main(String[] args) {
         Args args1 = new Args(8);
         args1.output();
+        args1.test("Jam");
     }
 }
---
+++ javaDaily/Basics/finally/FinallyExample.java
@@ -0,0 +1,14 @@
+public class FinallyExample {
+    public static void main(String[] args) {
+        try {
+            int res = 100/2;
+            System.out.println("Res : "+res);
+        }
+        catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        finally {
+            System.out.println("Its finallls");
+        }
+    }
+}
