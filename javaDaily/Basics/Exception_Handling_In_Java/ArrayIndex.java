fb029432dec332f7763ba6d45b45be40591ffec2
jojiseb <73522959+jojiseb@users.noreply.github.com>
2024-03-29 00:00:42 +0530

    ArrayIndexOutOfBounds explained

--- javaDaily/Basics/Exception_Handling_In_Java/ArrayIndex.java
+++ javaDaily/Basics/Exception_Handling_In_Java/ArrayIndex.java
@@ -1,12 +1,15 @@
 public class ArrayIndex {
     public static void main(String[] args) {
-        int nums[] = {1,2,3};
-        int index = 1090;
-
-        if(index == 0)
-            throw new ArrayIndexOutOfBoundsException("index can't be greater than lenght of array");
         try {
-            System.out.println(nums[index]);
+            int[] numbers = {1,2,3,4};
+            int index = 10;
+
+            if(index == 0 || index >= numbers.length) {
+                throw new ArrayIndexOutOfBoundsException("Index can't be 0 or greater than array length !"); //ArrayIndexOutOfBounds Exception got catched here
+            }       // So, Normal flow not interrupted
+            else{
+                System.out.println("Number : "+numbers[index]);
+            }
         }
         catch (Exception e) {
             System.out.println(e.getMessage());
