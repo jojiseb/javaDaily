417c5e4952c5f05ba77674020e273385e5077561
jojiseb <73522959+jojiseb@users.noreply.github.com>
2024-03-29 13:39:25 +0530

    Detail of how e.getMessage() got it value from - Cleared misunderstanding that what simply thrown is executed, but catch is performed , with clear message

--- javaDaily/Basics/Exception_Handling_In_Java/ArrayIndex.java
+++ javaDaily/Basics/Exception_Handling_In_Java/ArrayIndex.java
@@ -1,18 +1,24 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class ArrayIndex {
     public static void main(String[] args) {
         try {
-            int[] numbers = {1,2,3,4};
-            int index = 10;
+            int[] numbers = {11,27,30,42};
+            System.out.println(Arrays.toString(numbers));
+            System.out.println("Enter index : ");
+            Scanner scanner = new Scanner(System.in);
+            int index = scanner.nextInt();
 
-            if(index == 0 || index >= numbers.length) {
-                throw new ArrayIndexOutOfBoundsException("Index can't be 0 or greater than array length !"); //ArrayIndexOutOfBounds Exception got catched here
+            if(index < 0 || index >= numbers.length) {
+                throw new ArrayIndexOutOfBoundsException("Index can't be less than 0 or greater than array length !"); //ArrayIndexOutOfBounds Exception got catched here
             }       // So, Normal flow not interrupted
-            else{
-                System.out.println("Number : "+numbers[index]);
+            else {
+                System.out.println("Number : "+numbers[index]); //The message passed as parameter to above exception class is displayed as message in catch block..e.getMessage()
             }
         }
         catch (Exception e) {
-            System.out.println(e.getMessage());
+            System.out.println("Catching..."+e.getMessage());
         }
     }
 }
