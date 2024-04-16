c3145c2980a1f48810009b90ef6593f5bce32ffb
jojiseb <73522959+jojiseb@users.noreply.github.com>
2024-04-16 08:58:29 +0530

    Learn impact of using this keyword in toString()
    - Whenever an object is concatenated with a String, toString() is called
    - Causes StackOverflow error here, due to recursive calls to toString() implementation
    - toString() implementation here can be called here by : printing Object, using callCheck() which performs a concatenation
    - Avoid using this keyword in toString() implementation

--- javaDaily/Basics/Revision/ClassAndObject/MainPart4.java
+++ javaDaily/Basics/Revision/ClassAndObject/MainPart4.java
@@ -1,6 +1,3 @@
-import jdk.nashorn.internal.objects.annotations.Getter;
-import jdk.nashorn.internal.objects.annotations.Setter;
-
 import java.math.BigDecimal;
 
 class Money {
@@ -30,9 +27,16 @@ class Money {
     }
 
     public String toString() {
-//        return "Id : "+this.id+", Name : "+this.name+", Amount : "+this.amount;
         return "this : "+this+", id : "+this.id+", Name : "+this.name;
-    }
+    }   //Calling "this" will again cause a recursive call to toString() implementation, to convert object to a String
+        //before concatenating --> This will cause a recursion & eventually stackOverflow Error
+
+    //So, Don't concatenate "this" with a String in toString()
+
+    public String callCheck() {
+        System.out.println("Callcheck called..");
+        return this+" Hello"; //Concatenating "this" with a String will perform toString() call - Object code or overridden code
+    }                       //to convert object to String, before concatenating
 
 }
 
@@ -40,20 +44,25 @@ public class MainPart4 {
     public static void main(String[] args) {
         Money money = new Money(); //Initialisation of instance variables with default values
 
-        System.out.println(money);
+//        System.out.println(money);
 
         money.moneyDetails();
 
         Money money1 = new Money(2, BigDecimal.valueOf(100), "Dollar");
 
-        System.out.println(money1);
+//        System.out.println(money1);
 
         money1.moneyDetails();
 
         money.setAmount(BigDecimal.valueOf(500));
 
-        System.out.println(money);
+//        System.out.println(money);
 
         money.moneyDetails();
+
+//        System.out.println(money.callCheck());
+
+        int num = 89;
+        System.out.println(num);
     }
 }
