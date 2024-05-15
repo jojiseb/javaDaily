68f36e7cf39a18a93cf2af8cfcec4650645549ca
jojiseb <73522959+jojiseb@users.noreply.github.com>
2024-05-15 23:15:16 +0530

    - Example of Static inner class

--- javaDaily/Basics/StaticKeyword/StaticEx/InnerClasses2.java
+++ javaDaily/Basics/StaticKeyword/StaticEx/InnerClasses2.java
@@ -2,7 +2,7 @@ package StaticEx;
 
 public class InnerClasses2 {
     static class InnerTest { //Now, Inner Class is not dependent on Objects of Outer Class, but on Class itself
-        String name;I
+        String name;
         InnerTest(String name) {
             this.name = name;
         }
@@ -10,9 +10,10 @@ public class InnerClasses2 {
 
     public static void main(String[] args) {
         InnerTest it = new InnerTest("Abhi");   //Static inner class can create its object in main()
-        InnerTest it1 = new InnerTest("Sunny");
+        InnerTest it1 = new InnerTest("Sunny"); //A static inner class is a static member of outer class. It simply means Inner class can instantiate its
+        //its object without an instance of outer class.
 
-        System.out.println("1st Name : "+it.name); //Sunny
+        System.out.println("1st Name : "+it.name); //Abhi
         System.out.println("2nd Name : "+it1.name); //Sunny
     }
 }
