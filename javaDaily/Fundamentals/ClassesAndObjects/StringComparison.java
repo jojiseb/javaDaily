042fe54d00082c87b430e3ecd5933de9446335b0
jojiseb <73522959+jojiseb@users.noreply.github.com>
2024-08-02 23:53:56 +0530

    Practised some more of String comparisons, Did different types including compareTo(), Objects.equals() etc

--- javaDaily/Fundamentals/ClassesAndObjects/StringComparison.java
+++ javaDaily/Fundamentals/ClassesAndObjects/StringComparison.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+
 public class StringComparison {
     public static void main(String[] args) {
         String one = "One";
@@ -5,6 +7,12 @@ public class StringComparison {
         String three = new String("One");
         String four = one;
         String five =new String("One");
+
+        String six = "Ona"; //Gets allocated new m/y since no content exists before, and gets interned automatically by JVM
+
+        String seven = "Aone";
+
+        String eight = "Pna";
 
         //Here, When one is initialized in String pool, it looks whether the content
         //exists already ? If n't, It stores it and returns the reference.
@@ -59,6 +67,104 @@ public class StringComparison {
         else {
             System.out.println("N't Equal");
             System.out.println(hash);
+        }
+        System.out.println();
+        if(one.equals(three)) { //Equal since String.equals() compares by content
+            System.out.println("Equal");
+            System.out.println(hash);
+        }
+        else {
+            System.out.println("N't Equal");
+            System.out.println(hash);
+        }
+        System.out.println();
+
+        if(three.equals(five)) {    //Equal since String.equals() compares
+            System.out.println("Equal"); //by content, even if it's a String literal or uses
+            System.out.println(hash); // new keyword
+        }
+        else {
+            System.out.println("N't Equal");
+            System.out.println(hash);
+        }
+
+        System.out.println();
+        if(Objects.equals(one, five)) { //Perform null check and then compares content
+            System.out.println("Equal"); //Normally used to make sure Objects are null-safe while comparing
+        }
+        else {
+            System.out.println("N't Equal");
+        }
+
+        System.out.println();
+        if(Objects.equals(null, null)) {
+            System.out.println("Equal");
+        }
+        else {
+            System.out.println("N't Equal");
+        }
+
+        System.out.println();
+        if(Objects.equals(one, null)) {
+            System.out.println("Equal");
+        }
+        else {
+            System.out.println("N't Equal");
+        }
+
+        System.out.println();
+
+        int res = one.compareTo(two); //0 , since both are lexicograhically same
+
+        if(res > 0) {
+            System.out.println("one > two");
+        }
+        else if(res == 0) {
+            System.out.println("one = two");
+        }
+        else {
+            System.out.println("one < two");
+        }
+
+        System.out.println();
+
+        int r1 = one.compareTo(six); //e in One is > a in Ona
+
+
+        if(r1 > 0) {
+            System.out.println("one > six");
+        }
+        else if(r1 == 0) {
+            System.out.println("one = six");
+        }
+        else {
+            System.out.println("one < six");
+        }
+
+        System.out.println();
+
+        int r2 = one.compareTo(seven); //O in One is > A in Aone
+
+        if(r2 > 0) {
+            System.out.println("one > seven");
+        }
+        else if(r2 == 0) {
+            System.out.println("one = seven");
+        }
+        else {
+            System.out.println("one < seven");
+        }
+
+        int r3 = one.compareTo(eight); //o in One < P in Pna
+
+        if(r3 > 0) {
+            System.out.println("One > Pna");
+        }
+        else if(r3 == 0) {
+            System.out.println("One = Pna");
+        }
+        else {
+            System.out.println("One < Pna"); //1st Character in 2nd String > 1st Character in 1st String
         }
     }
 }
